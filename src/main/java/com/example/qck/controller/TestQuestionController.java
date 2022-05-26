96050e6d0b334b5ea8b6a69562b82ea3c297b365
mauyez <mauyez@ikod.kz>
2022-05-26 15:52:30 +0600

    added test question, question type; small changes to learning objective repository due to test question

--- src/main/java/com/example/qck/controller/TestQuestionController.java
+++ src/main/java/com/example/qck/controller/TestQuestionController.java
@@ -2,7 +2,6 @@ package com.example.qck.controller;
 
 import com.example.qck.model.TestQuestion;
 import com.example.qck.repository.LearningObjectiveRepository;
-import com.example.qck.repository.TestQuestionRepository;
 import com.example.qck.service.TestQuestionService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestQuestionController {
 
     private final TestQuestionService testQuestionService;
-    private final TestQuestionRepository questionRepository;
     private final LearningObjectiveRepository learningObjectiveRepository;
 
     @GetMapping("/new")
@@ -31,18 +29,6 @@ public class TestQuestionController {
 
     @PostMapping("/saveTestQuestion")
     public String saveTestQuestion(@ModelAttribute("testQuestion") TestQuestion testQuestion){
-        System.out.println(testQuestion.getId());
-        System.out.println(testQuestion.getQuestion());
-        System.out.println(testQuestion.getType().name());
-        System.out.println(testQuestion.getQuestionOption().getOptionA());
-        System.out.println(testQuestion.getQuestionOption().getOptionB());
-        System.out.println(testQuestion.getQuestionOption().getOptionC());
-        System.out.println(testQuestion.getQuestionOption().getOptionD());
-        System.out.println(testQuestion.getCorrectAnswer());
-        System.out.println(testQuestion.getLearningObjective().getId());
-        System.out.println(testQuestion.getLearningObjective().getSubject().getName());
-        System.out.println(testQuestion.getLearningObjective().getName());
-
         testQuestionService.saveTestQuestion(testQuestion);
         return "redirect:/";
     }
