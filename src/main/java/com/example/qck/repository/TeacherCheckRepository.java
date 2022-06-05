fdfef51e506d92ee9b083fd89ba86c4e3550f984
mauyez <mauyez@ikod.kz>
2022-06-05 17:04:39 +0600

    localhost db commented

--- src/main/java/com/example/qck/repository/TeacherCheckRepository.java
+++ src/main/java/com/example/qck/repository/TeacherCheckRepository.java
@@ -4,4 +4,5 @@ import com.example.qck.model.TeacherCheck;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeacherCheckRepository extends JpaRepository<TeacherCheck, Long> {
+    TeacherCheck getTeacherCheckByStudentTestAttemptIdAndTeacherId(Long studentTestAttemptId, Long teacherId);
 }
